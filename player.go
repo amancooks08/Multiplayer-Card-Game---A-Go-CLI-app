d39578284ab9201bc13566550416d574c8147b3d
Amandeep Singh <amandeepsingh.chhabra08@gmail.com>
2023-04-11 13:23:49 +0530

    add reversePlayerOrder and next player function

--- player.go
+++ player.go
@@ -5,3 +5,26 @@ type Player struct {
 	Hand  []Card
 	Score int
 }
+
+// getNextPlayer returns the next player in the turn sequence
+func getNextPlayer(players []Player, currentPlayer *Player) *Player {
+	currentPlayerIdx := -1
+	for i := 0; i < len(players); i++ {
+		if players[i].Name == currentPlayer.Name {
+			currentPlayerIdx = i
+			break
+		}
+	}
+	if currentPlayerIdx == -1 {
+		return nil
+	}
+
+	nextPlayerIdx := (currentPlayerIdx + 1) % len(players)
+	return &players[nextPlayerIdx]
+}
+
+func reversePlayerOrder(players []Player) {
+	for i, j := 0, len(players)-1; i < j; i, j = i+1, j-1 {
+		players[i], players[j] = players[j], players[i]
+	}
+}
